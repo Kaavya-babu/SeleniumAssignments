b4a9cd0f46126610d44a7286d9b7a9c8a4d436d3
Kaavya-babu <117551917+Kaavya-babu@users.noreply.github.com>
2022-11-13 10:11:32 +0530

    Add files via upload

---
+++ week2/day1/DuplicateLead.java
@@ -0,0 +1,55 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DuplicateLead {
+
+	public static void main(String[] args) throws InterruptedException {
+WebDriverManager.chromedriver().setup();
+        
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf.org");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Kaavya");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Babu");
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Kaavyadharshu");
+		driver.findElement(By.name("departmentName")).sendKeys("QA");
+		driver.findElement(By.name("description")).sendKeys("DEscription is added ");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("kaavyanermala51997.kn@gmail.com");
+		
+		Thread.sleep(2000);
+		WebElement dropd= driver.findElement(By.name("generalStateProvinceGeoId"));
+		Select dropdown= new Select(dropd)  ;
+		dropdown.selectByVisibleText("New York");
+		driver.findElement(By.name("submitButton")).click();		
+		String titleofEditLead= driver.getTitle();
+		System.out.println("The title is:: "+ titleofEditLead);
+		
+		driver.findElement(By.linkText("Duplicate Lead")).click();
+		driver.findElement(By.id("createLeadForm_firstName")).clear();
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("New Kaavya");
+		driver.findElement(By.className("smallSubmit")).click();
+		String titleofDuplicateLead= driver.getTitle();
+		System.out.println("The title of Duplicate Lead is:: "+ titleofDuplicateLead);
+		
+		
+		
+		
+		driver.close();
+
+	}
+
+}
